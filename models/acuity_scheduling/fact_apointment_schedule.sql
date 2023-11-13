67042c0e053dbd09438201dc20357043254026ba
hantypen <antypenko.hanna@gmail.com>
2023-11-13 16:40:34 +0200

    add

--- models/acuity_scheduling/fact_apointment_schedule.sql
+++ models/acuity_scheduling/fact_apointment_schedule.sql
@@ -16,9 +16,20 @@ find_dublicates as (
 select 
     *,
     row_number() over (partition by appointment_id order by ingested_at_timestamp desc) as rnk
-from added_and_edited)
+from added_and_edited),
 
-select * from find_dublicates
+cancelled as (
+    select appointment_id, true as cancelled_flag
+    from {{ ref('stg_appointment_deleted_event') }}
+)
+
+select {{ dbt_utils.star(from=ref('stg_appointment_added_event'), relation_alias='find_dublicates') }},
+        cancelled.cancelled_flag
+
+ from find_dublicates
+
+left join cancelled
+on cancelled.appointment_id = find_dublicates.appointment_id
 where rnk = 1
 
 
